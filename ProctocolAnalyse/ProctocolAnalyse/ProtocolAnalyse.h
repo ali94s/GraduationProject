52874751dfd60965d9656a57bf088f18855b9f56
ali94s <ali_netmail@163.com>
2017-05-03 17:39:34 +0800

    建立处理 会话超时的结构 程序可以识别出协议，但是统计未能做好

--- ProctocolAnalyse/ProctocolAnalyse/ProtocolAnalyse.h
+++ ProctocolAnalyse/ProctocolAnalyse/ProtocolAnalyse.h
@@ -3,11 +3,26 @@
 #include<pcap/pcap.h>
 #include"libndpi-1.8.0/libndpi/ndpi_main.h"
 #include"libndpi-1.8.0/libndpi/ndpi_api.h"
+#include<signal.h>
+#include<sys/time.h>
+#include<pthread.h>
+
+
 #define IP_MF 0x2000     //0010 0000 0000 0000
 #define IP_OFFSET 0x1fff    //offset part
 #define SNAP 0xaa
 
+//main struct 
+struct ndpi_workflow
+{
+	//u_int32_t last time;
+	pcap_t *handle;
+	struct ndpi_detection_module_struct *ndpi_struct;
+	void **ndpi_flow_root; 
+};
 
+
+//ndpi flow infstruct
 struct ndpi_flow_info
 {
 	u_int32_t src;
@@ -18,7 +33,69 @@ struct ndpi_flow_info
 	struct ndpi_flow_struct *ndpi_flow;
 	ndpi_protocol detected_protocol;
 	void *src_id,*dst_id;
+	u_int32_t packets;
+	int data_len;
+	struct timeval begin,end;
 };
+
+
+
+
+//some struct for ip regroup
+struct sk_buff
+{
+	char *data;
+	int truesize;
+};
+
+
+struct hostfrags
+{
+	struct ipq *ipqueue;  //ip fragments queue
+	int ip_frag_mem;
+	u_int ip;
+	int hash_index;
+	struct hostfrags *prev;
+	struct hostfrags *next;
+};
+
+struct ipfrag
+{
+	int offset;
+	int end;
+	int len;
+	struct sk_buff *skb;
+	unsigned char *ptr;
+	struct ipfrag *prev;
+	struct ipfrag *next;
+};
+
+struct timer_list
+{
+	struct timer_list *prev;
+	struct timer_list *next;
+	int expires;
+	void (*function)();
+	unsigned long data;
+};
+
+
+struct ipq
+{
+	u_char *mac;
+	struct ndpi_iphdr *iph;
+	int32_t len;
+	int16_t iplen;
+	int16_t maclen;
+	struct timer_list timer;
+	struct ipfrag *fragments;
+	struct hostfrag *hf;
+	struct ipq *prev;
+	struct ipq *next;
+};
+
+
+
 
 
 /*
@@ -76,7 +153,7 @@ void run_pcaploop(struct pcap_t *device);
 void packet_analyse(u_char *user, const struct pcap_pkthdr *hdr, const u_char *packet);
 
 //get protocol func
-ndpi_protocol  get_protocol(struct ndpi_iphdr *hdr,u_int16_t ip_offset,u_int32_t ip_size);
+void get_protocol(struct ndpi_iphdr *hdr,u_int16_t ip_offset,u_int32_t ip_size);
 
 //init workflow
 void ndpi_workflow_init(pcap_t *handle);
@@ -92,3 +169,44 @@ int ndpi_node_com(const void *a,const void *b);
 
 //regroup ip fragments
 struct ndpi_iphdr* get_whole_ip_packet(struct ndpi_iphdr* iph);
+
+//ip_defrag 
+char* ip_defrag(struct ndpi_iphdr *iph,struct sk_buff *skb);
+
+//hostfrag_find
+int hostfrag_find(struct ndpi_iphdr* iph);
+
+//calc hash index
+int frag_index(struct ndpi_iphdr *iph);
+
+
+//creat hostfrag
+void hostfrag_create(struct ndpi_iphdr* iph);
+
+void ip_evictor(void);
+void kfree_skb(struct sk_buff *skb);
+void ip_free(struct ipq * qp);
+void del_timer(struct timer_list * x);
+void frag_kfree_skb(struct sk_buff * skb);
+void frag_kfree_s(void *ptr, int len);
+void atomic_sub(int ile, int *co);
+struct ipq *ip_find(struct ndpi_iphdr*iph);
+int jiffies();
+void ip_expire(unsigned long arg);
+void add_timer(struct timer_list * x);
+struct ipq* ip_create(struct ndpi_iphdr *iph);
+void *frag_kmalloc(int size);
+void atomic_add(int ile, int *co);
+struct ipfrag *ip_frag_create(int offset, int end, struct sk_buff * skb, unsigned char *ptr);
+int ip_done(struct ipq * qp);
+char *ip_glue(struct ipq * qp);
+
+
+
+//pthread
+void init_time();
+void init_sigaction();
+void thread_fun(void *arg);
+void do_timeout();
+
+
--- ProctocolAnalyse/ProctocolAnalyse/main.c
+++ ProctocolAnalyse/ProctocolAnalyse/main.c
@@ -1,36 +1,29 @@
 #include"ProtocolAnalyse.h"
+//#include<signal.h>
+//#include<sys/time.h>
+//#include<unistd.h>
+
 //#define SIZEOF_ID_STRUCT (sizeof(struct ndpi_id_struct))
 //struct ndpi_flow_struct *ndpi_flow;
 //void *src,*dst;
+
+
+
+
 int main()
 {
-/*	
-	ndpi_flow = (struct ndpi_flow_struct*)malloc(sizeof(struct ndpi_flow_struct));
-	if(ndpi_flow==NULL)
-	{
-		printf("malloc failed\n");
-		return 0;
-	}
-	memset(ndpi_flow,0,sizeof(struct ndpi_flow_struct));
-	src = ndpi_malloc(SIZEOF_ID_STRUCT);
-	if(src == NULL)
-	{
-		return 0;
-	}
-	memset(src,0,SIZEOF_ID_STRUCT);
-	dst = ndpi_malloc(SIZEOF_ID_STRUCT);
-	if(dst == NULL)
-	{
-		return 0;
-	}
-	memset(dst,0,SIZEOF_ID_STRUCT);
-*/	
 	
+
 	struct pcap_t *device = open_pcapdevice();
 	
 	//init ndpi struct
 	setup_detection(device);
 
+	//timeout system
+	//init_sigaction();
+	//init_time();
+	thread_create();
+	
 	//run pcap_loop
 	run_pcaploop(device);
 	
